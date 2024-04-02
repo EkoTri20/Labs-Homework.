f6f47004e7d8845e2965b9ca6c0ad82921896c1d
郭成文 <162436360+EkoTri20@users.noreply.github.com>
2024-04-02 10:54:06 +0800

    Add files via upload

---
+++ JPA01.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class JPA01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Input: ");
+        int n = scanner.nextInt();
+
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+
+        System.out.println("1+...+" + n + " = " + sum);
+
+        scanner.close();
+    }
+}
