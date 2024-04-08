372f85762c856cb2e56e86049296c96079690e59
郭成文 <162436360+EkoTri20@users.noreply.github.com>
2024-04-08 23:18:13 +0800

    Update JPA-401.js

--- JPA-401.js
+++ JPA-401.js
@@ -2,35 +2,26 @@ import java.util.Scanner;
 
 public class JPA401 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        // 建立一個長度為10的整數陣列
-        int[] numbers = new int[10];
-
-        // 提示用戶輸入10個整數
+        Scanner scn = new Scanner(System.in);
+        int[] numberArray = new int[11];
+        int listTotal = 0;
+        int listCount = 0;
         System.out.println("請輸入10個整數:");
-
-        // 依次要求用戶輸入10個整數
-        for (int i = 0; i < 10; i++) {
-            System.out.print("第" + (i + 1) + "個整數: ");
-            numbers[i] = scanner.nextInt();
-        }
-
-        // 計算大於60的數量、總和和平均值
-        int countOver60 = 0;
-        int sumOver60 = 0;
-        for (int number : numbers) {
-            if (number > 60) {
-                countOver60++;
-                sumOver60 += number;
+        for (int i = 1; i < numberArray.length; i++) {
+            System.out.print("第"+ i +"個整數:");
+            numberArray[i] = scn.nextInt();
+            if (numberArray[i] > 60){
+                listTotal += numberArray[i];
+                listCount++;
             }
         }
+        System.out.println("陣列中大於60的有" + listCount + "個");
+        System.out.println("綜合為" + listTotal);
+        System.out.printf("平均值為%f",  ((double)listTotal / listCount));
+        System.out.println("");
+        scn.close();
 
-        // 顯示結果
-        System.out.println("陣列中大於60的有" + countOver60 + "個");
-        System.out.println("總合為" + sumOver60);
-        System.out.println("平均值為" + (double) sumOver60 / countOver60);
-
-        scanner.close();
     }
+
+    
 }
